cb5df0987d535566997ff72c82aa94e783603b17
ifuad <i.fuad.tm@gmail.com>
2026-04-07 21:58:31 +0400

    render fix

--- backend/app.py
+++ backend/app.py
@@ -175,4 +175,6 @@ def serve_frontend(path=''):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_DEBUG', '1') == '1'
+    app.run(host='0.0.0.0', port=port, debug=debug)
